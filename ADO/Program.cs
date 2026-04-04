0fa4a7e625d92d12f258a088e9497741be9f3fcb
AfanasevStas <afanasevstas96@mail.ru>
2026-04-04 20:16:38 +0300

    Insert DONE

--- ADO/Program.cs
+++ ADO/Program.cs
@@ -20,7 +20,9 @@ namespace ADO
 
             //Console.WriteLine(connector.GetPrimaryKeyColumnName("Movies"));
             Console.WriteLine(connector.GetNextPrimaryKey("Movies"));
-            connector.Insert($"INSERT Directors(director_id,first_name,last_name)VALUES({connector.GetNextPrimaryKey("Directors")},N'Peter',N'Jackson')");
+            //connector.Insert($"INSERT Directors(director_id,first_name,last_name)VALUES({connector.GetNextPrimaryKey("Directors")},N'Peter',N'Jackson')");
+
+            connector.Insert("Directors","director_id,first_name,last_name",$"{connector.GetNextPrimaryKey("Directors")},N'Peter',N'Jackson'");
 
             connector.Select("SELECT * FROM Directors");
             connector.Select("mouvie_id,title,first_name,last_name","Movies,Directors","director=director_id");
